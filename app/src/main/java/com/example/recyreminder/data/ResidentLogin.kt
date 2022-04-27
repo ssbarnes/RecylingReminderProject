3e775223ad4c22b7479561b174dbe47211044640
Scott Barnes <sbarnes8@terpmail.umd.edu>
2022-04-26 23:45:59 -0500

    Resident Login Page

--- app/src/main/java/com/example/recyreminder/data/ResidentLogin.kt
+++ app/src/main/java/com/example/recyreminder/data/ResidentLogin.kt
@@ -1,4 +1,42 @@
 package com.example.recyreminder.data
 
-class ResidentLogin {
+import android.app.Activity
+import android.os.Bundle
+import android.content.Intent
+import android.text.Editable
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import com.example.recyreminder.R
+import java.util.*
+
+class ResidentLogin : Activity() {
+
+    private lateinit var login : Button
+    private lateinit var user : EditText
+    private lateinit var pass : EditText
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.residents_login)
+
+        login = findViewById(R.id.resLogin)
+        login.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                user = findViewById(R.id.resUsername)
+                pass = findViewById(R.id.resPassword)
+                login(user.text.toString(), pass.text.toString())
+            }
+        })
+
+    }
+
+    fun login(username: String, password: String) {
+        //TODO - Connect to firebase,
+        // check if username and password exists in it, then switch activities
+        Log.i("tag", username)
+        Log.i("tag", password)
+    }
 }

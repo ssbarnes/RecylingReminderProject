96d3db5f387e81ac51f0ef9ebd3658cf740e83e4
MSub1 <90069195+MSub1@users.noreply.github.com>
2022-04-26 22:43:50 -0400

    Adding Selection Page, GC Company, and Login Layout

---
+++ app/src/main/java/com/example/recyreminder/SelectPage.kt
@@ -0,0 +1,20 @@
+package com.example.recyreminder
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+
+class FirstPage: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.first_page)
+
+        Log.i(TAG)
+    }
+
+    companion object {
+        private val TAG = "Select Page"
+    }
+
+}

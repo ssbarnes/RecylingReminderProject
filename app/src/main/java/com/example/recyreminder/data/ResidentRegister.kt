2751cc23d89c8f80c0d30de4304e22a4689af03e
MSub1 <90069195+MSub1@users.noreply.github.com>
2022-04-30 12:49:30 -0400

    Adding files for registration, login colors, gc main menu

--- app/src/main/java/com/example/recyreminder/data/ResidentRegister.kt
+++ app/src/main/java/com/example/recyreminder/data/ResidentRegister.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.example.recyreminder.R
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -24,42 +25,56 @@ class ResidentRegister : Activity() {
     private lateinit var user : EditText
     private lateinit var pass : EditText
     private lateinit var address : EditText
+    private lateinit var city : EditText
+    private lateinit var countryState : EditText
+    private lateinit var zipCode : EditText
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.residents_register)
+
+        Log.i(TAG, "Entered Resident Registration")
 
         register = findViewById(R.id.resRegRegister)
         register.setOnClickListener {
             user = findViewById(R.id.resRegUsername)
             pass = findViewById(R.id.resRegPassword)
             address = findViewById(R.id.resRegAddress)
+            city = findViewById(R.id.resRegCity)
+            countryState = findViewById(R.id.resRegCountryState)
+            zipCode = findViewById(R.id.resRegZipCode)
 
-            sendData(user.text.toString(), pass.text.toString(), address.text.toString())
+            val newAddress = address.text.toString() + ", " + city.text.toString() + ", " +
+                    countryState.text.toString() + " " + zipCode.text.toString()
+
+            sendData(user.text.toString(), pass.text.toString(), newAddress)
+            finish()
         }
 
     }
 
     fun sendData(username: String, password: String, addr: String) {
         //TODO - post data to firebase database
-        Log.i(TAG, username)
-        Log.i(TAG, password)
-        Log.i(TAG, addr)
+//        Log.i(TAG, username)
+//        Log.i(TAG, password)
+//        Log.i(TAG, addr)
 
         val newUser: MutableMap<String, Any> = HashMap()
         newUser["username"] = username
         newUser["password"] = password
         newUser["address"] = addr
+        newUser["position"] = "resident"
 
         val database = Firebase.database.reference
         val usersRef: DatabaseReference = database.child("users")
-        val userRef = usersRef.child(username)
+        val userRef = usersRef.child(addr)
 
         userRef.addListenerForSingleValueEvent(object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 if (snapshot.exists()) {
                     // TODO - tell user they're already registered
                     Log.i(TAG, "Uh oh username already exists")
+                    Toast.makeText(applicationContext, "Username already exists", Toast.LENGTH_SHORT)
                 } else {
                     Log.i(TAG, "Come on in")
                     userRef.setValue(newUser)
@@ -69,6 +84,8 @@ class ResidentRegister : Activity() {
                 Log.i(TAG, error.message)
             }
         })
+
+        Log.i(TAG, "Completed Resident Registration")
     }
 
     companion object {
--- app/src/main/java/com/example/recyreminder/data/SelectPage.kt
+++ app/src/main/java/com/example/recyreminder/data/SelectPage.kt
@@ -1,4 +1,4 @@
-package com.example.recyreminder
+package com.example.recyreminder.data
 
 import android.app.Activity
 import android.content.Intent
@@ -6,9 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import com.example.recyreminder.data.PositionLogin
-import com.example.recyreminder.ui.login.LoginActivity
-import java.io.*
+import com.example.recyreminder.R
 
 class SelectPage: Activity() {
 
